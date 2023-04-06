3814abdffde7a8447e0659204db3ce17a1f62c67
Jason Barry <jb@netlify.com>
2023-04-06 13:03:28 -0700

    Parameterize chat edge function

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # chatgpt-alpinejs-netlify-openai
+
 ChatGPT style web app(alpine.js) using Netlify Edge Functions Streaming
 
+[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/netlify/openai-edge-functions)
+
 ## Setup:
+
 1. Clone this repo
 2. Get OpenAI API Key (You'll need an OpenAI account)
 3. Connect your repo in Netlify (You'll need a Netlify account)
-3. Setup a site in Netlify by connecting to your repo
-4. Before you build, setup the env variable OPENAI_API_KEY for your site
-5. That's all! Enjoy the streaming chat experience.
+4. Setup a site in Netlify by connecting to your repo
+5. Before you build, setup the env variable OPENAI_API_KEY for your site
+6. That's all! Enjoy the streaming chat experience.
 
 Note: This demo is built to show the power of Netlify Edge Functions and how easy its to build streaming applications. It was great using ChatGPT-4 to help me with initial set of code, though I did a good bit of improvisation along the way. Please do share use cases you're building with generative AI APIs.
---
+++ netlify/edge-functions/chat.ts
@@ -0,0 +1,82 @@
+import type { Config, Context } from "https://edge.netlify.com";
+
+interface Params {
+  model: "gpt-4" | "gpt-3.5-turbo";
+  messages: Array<{
+    role: "system" | "user" | "assistant";
+    content: string;
+  }>;
+  max_tokens: number;
+  temperature: number;
+  top_p: number;
+  frequency_penalty: number;
+  presence_penalty: number;
+  stream: boolean;
+}
+
+export default async (req: Request, context: Context) => {
+  const text = await req.text();
+  const text_json = JSON.parse(text);
+  console.log(JSON.stringify(text_json));
+  const userMessage = text_json["message"] || "hello";
+  const streamType = JSON.parse(text_json["stream"]);
+
+  const API_URL = "https://api.openai.com/v1/chat/completions";
+  // @ts-expect-error
+  const OPENAI_API_KEY = Deno.env.get("OPENAI_API_KEY");
+
+  const url = new URL(req.url);
+  const params = url.searchParams;
+
+  const bodyParams: Params = {
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content: userMessage,
+      },
+    ],
+    max_tokens: Number(params.get("max_tokens")) || 512,
+    temperature: Number(params.get("temperature")) || 0.2,
+    top_p: Number(params.get("top_p")) || 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    stream: streamType,
+  };
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${OPENAI_API_KEY}`,
+    },
+    body: JSON.stringify(bodyParams),
+  };
+  console.log("calling OpenAI API: " + JSON.stringify(bodyParams));
+  try {
+    const res = await fetch(API_URL, requestOptions);
+
+    if (!res.ok) {
+      const errorMessage = await res.text();
+      console.error(
+        `OpenAI API Error: ${res.status} ${res.statusText} - ${errorMessage}`
+      );
+      throw new Error(`OpenAI API Error: ${res.status} ${res.statusText}`);
+    }
+
+    console.log("Response back from OpenAI");
+    console.log(res.status);
+    console.log(res.statusText);
+    console.log(res);
+    return new Response(res.body, {
+      status: res.status,
+      statusText: res.statusText,
+      headers: { "Content-Type": "text/plain" },
+    });
+  } catch (error) {
+    console.error("Error while processing request:", error);
+    return new Response(error.message || "Internal server error", {
+      status: 500,
+    });
+  }
+};
