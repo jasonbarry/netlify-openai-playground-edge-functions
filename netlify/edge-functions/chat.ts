fca5fdaab9fb3ceaaf54b17ab315a762c1e95ff8
Jason Barry <jb@netlify.com>
2023-04-06 13:10:09 -0700

    Parameterize more options

--- netlify/edge-functions/chat.ts
+++ netlify/edge-functions/chat.ts
@@ -1,7 +1,8 @@
 import type { Config, Context } from "https://edge.netlify.com";
 
+type Model = "gpt-4" | "gpt-3.5-turbo";
 interface Params {
-  model: "gpt-4" | "gpt-3.5-turbo";
+  model: Model;
   messages: Array<{
     role: "system" | "user" | "assistant";
     content: string;
@@ -29,7 +30,7 @@ export default async (req: Request, context: Context) => {
   const params = url.searchParams;
 
   const bodyParams: Params = {
-    model: "gpt-3.5-turbo",
+    model: (params.get("model") as Model) || "gpt-3.5-turbo",
     messages: [
       {
         role: "user",
@@ -39,8 +40,8 @@ export default async (req: Request, context: Context) => {
     max_tokens: Number(params.get("max_tokens")) || 512,
     temperature: Number(params.get("temperature")) || 0.2,
     top_p: Number(params.get("top_p")) || 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
+    frequency_penalty: Number(params.get("frequency_penalty")) || 0,
+    presence_penalty: Number(params.get("presence_penalty")) || 0,
     stream: streamType,
   };
 
