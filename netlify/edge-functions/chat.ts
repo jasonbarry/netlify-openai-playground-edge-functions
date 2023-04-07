58441ff8ff363104cc8300c7f81a0e3304002dcf
Jason Barry <jb@netlify.com>
2023-04-07 14:57:29 -0700

    Rearrange some folders

--- netlify/edge-functions/chat.ts
+++ netlify/edge-functions/chat.ts
@@ -1,48 +1,36 @@
-import type { Config, Context } from "https://edge.netlify.com";
+import type { Config, Context } from "netlify:edge";
 
-type Model = "gpt-4" | "gpt-3.5-turbo";
 interface Params {
-  model: Model;
+  model?: "gpt-4" | "gpt-3.5-turbo";
   messages: Array<{
     role: "system" | "user" | "assistant";
     content: string;
   }>;
-  max_tokens: number;
-  temperature: number;
-  top_p: number;
-  frequency_penalty: number;
-  presence_penalty: number;
-  stream: boolean;
+  max_tokens?: number;
+  temperature?: number;
+  top_p?: number;
+  frequency_penalty?: number;
+  presence_penalty?: number;
+  stream?: boolean;
 }
 
 export default async (req: Request, context: Context) => {
   const text = await req.text();
-  const text_json = JSON.parse(text);
-  console.log(JSON.stringify(text_json));
-  const userMessage = text_json["message"] || "hello";
-  const streamType = JSON.parse(text_json["stream"]);
+  const params: Params = JSON.parse(text);
 
   const API_URL = "https://api.openai.com/v1/chat/completions";
   // @ts-expect-error
   const OPENAI_API_KEY = Deno.env.get("OPENAI_API_KEY");
 
-  const url = new URL(req.url);
-  const params = url.searchParams;
-
   const bodyParams: Params = {
-    model: (params.get("model") as Model) || "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "user",
-        content: userMessage,
-      },
-    ],
-    max_tokens: Number(params.get("max_tokens")) || 512,
-    temperature: Number(params.get("temperature")) || 0.2,
-    top_p: Number(params.get("top_p")) || 1,
-    frequency_penalty: Number(params.get("frequency_penalty")) || 0,
-    presence_penalty: Number(params.get("presence_penalty")) || 0,
-    stream: streamType,
+    model: params.model || "gpt-3.5-turbo",
+    messages: params.messages,
+    max_tokens: params.max_tokens || 256,
+    temperature: params.temperature || 0.7,
+    top_p: params.top_p || 1,
+    frequency_penalty: params.frequency_penalty || 0,
+    presence_penalty: params.presence_penalty || 0,
+    stream: params.stream ?? true,
   };
 
   const requestOptions = {
@@ -53,7 +41,9 @@ export default async (req: Request, context: Context) => {
     },
     body: JSON.stringify(bodyParams),
   };
+
   console.log("calling OpenAI API: " + JSON.stringify(bodyParams));
+
   try {
     const res = await fetch(API_URL, requestOptions);
 
@@ -65,10 +55,8 @@ export default async (req: Request, context: Context) => {
       throw new Error(`OpenAI API Error: ${res.status} ${res.statusText}`);
     }
 
-    console.log("Response back from OpenAI");
-    console.log(res.status);
-    console.log(res.statusText);
-    console.log(res);
+    console.log("Response back from OpenAI", res.body);
+
     return new Response(res.body, {
       status: res.status,
       statusText: res.statusText,
